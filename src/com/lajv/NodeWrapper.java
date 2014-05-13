50b585333fb50cd61b6ddc6a659c5ab9db35e56d
Jimmy Zöger <jimmy.zoger@gmail.com>
2014-05-13 11:27:39 +0200

    Merges NodeWrapper to contain attributes for ClosePeer

--- src/com/lajv/NodeWrapper.java
+++ src/com/lajv/NodeWrapper.java
@@ -1,10 +1,14 @@
 package com.lajv;
 
+import com.lajv.vivaldi.VivaldiCoordinate;
+
 import peersim.core.Node;
 
 public class NodeWrapper {
 	public Node node;
 	public int age;
+	public VivaldiCoordinate coord;
+	public double distance;
 
 	public NodeWrapper(Node node) {
 		this.node = node;
@@ -17,8 +21,15 @@ public class NodeWrapper {
 		return nw;
 	}
 
+	public NodeWrapper closePeerCopy() {
+		NodeWrapper copy = new NodeWrapper(node);
+		copy.coord = (VivaldiCoordinate) coord.clone();
+		copy.distance = distance;
+		return copy;
+	}
+
 	@Override
 	public String toString() {
-		return "("+node.getID()+")";
+		return "(" + node.getID() + ")";
 	}
 }
